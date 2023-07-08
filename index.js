06865dc04a1beb6b1d43bf4b4dd3dcb0be152c43
Sau0908 <kashyapsaurabh0908@gmail.com>
2023-07-08 08:17:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("This is a Memories App");
+});
+
+// app.get("/posts", (req, res) => {
+//   res.send("This Works");
+// });
+app.use("/posts", postRoutes);
+//const CONNECTION_URL =
+// "mongodb+srv://saurabhkashyap982000:saurabhasadmin@cluster0.kmlx4h9.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
